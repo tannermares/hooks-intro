c9a55875d9e4dfa482ed060850b9d8e00e371023
Tanner Mares <tannermares@gmail.com>
2019-09-18 09:48:48 -0700

    fixed name

--- src/functions/HelloWorld.js
+++ src/functions/HelloWorld.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import Row from '../Row'
 
 export default function HelloWorld(props) {
-  const [name, setName] = useState('Han')
+  const [name, setName] = useState('Han Solo')
 
   function handleNameChange(e) {
     setName(e.target.value)
