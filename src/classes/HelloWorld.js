11b42fcee346e79f0b5213b707faee838602c22f
Tanner Mares <tannermares@gmail.com>
2019-09-18 09:50:36 -0700

    added second state to class

--- src/classes/HelloWorld.js
+++ src/classes/HelloWorld.js
@@ -6,18 +6,28 @@ export default class HelloWorld extends React.Component {
     super(props)
     this.state = {
       name: 'Han Solo',
+      job: 'Smuggler',
     }
     this.handleNameChange = this.handleNameChange.bind(this)
+    this.handleJobChange = this.handleJobChange.bind(this)
   }
 
   handleNameChange(e) {
     this.setState({ name: e.target.value })
+  }
+
+  handleJobChange(e) {
+    this.setState({ job: e.target.value })
   }
   render() {
     return (
       <section>
         <Row label="Name">
           <input value={this.state.name} onChange={this.handleNameChange} />
+        </Row>
+
+        <Row label="Job">
+          <input value={this.state.job} onChange={this.handleJobChange} />
         </Row>
       </section>
     )
