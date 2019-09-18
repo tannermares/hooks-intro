3655c5a3dc44697a73cb972403374b456876e3de
Tanner Mares <tannermares@gmail.com>
2019-09-18 09:58:34 -0700

    added effect to function

--- src/functions/HelloWorld.js
+++ src/functions/HelloWorld.js
@@ -1,9 +1,13 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Row from '../Row'
 
 export default function HelloWorld(props) {
   const [name, setName] = useState('Han Solo')
   const [job, setJob] = useState('Smuggler')
+
+  useEffect(() => {
+    document.title = name + ' ' + job
+  })
 
   function handleNameChange(e) {
     setName(e.target.value)
